0184ff6f430625d65ccccaed005f74bb8f2116de
Felipe Teixeira <felipesantosteixeira@outlook.com>
2019-02-03 10:01:10 -0200

    metrics list and icon help

--- src/app/shared/metrics/metrics/metrics.component.ts
+++ src/app/shared/metrics/metrics/metrics.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+const metricsList = [
+    { name: 'Total de Clientes', val: '2547', percentage: '77%', icon: 'users', isUp: true },
+    { name: 'Total de vendas', val: '3.427,00', percentage: '6', icon: 'buy', isDown: true },
+    { name: 'Total de compartilhamentos', val: '2.547.251', percentage: '75%', icon: 'shared', isUp: true },
+    { name: 'Receita em WiBX', val: '890,50', percentage: '12%', icon: 'dollar', isDown: true },
+    { name: 'Saldo em WiBX', val: '1.547.234', percentage: null, icon: 'wallet', totalWibx: '2.547.234' },
+];
+
 @Component({
-  selector: 'app-metrics',
-  templateUrl: './metrics.component.html',
-  styleUrls: ['./metrics.component.scss']
+    selector: 'app-metrics',
+    templateUrl: './metrics.component.html',
+    styleUrls: ['./metrics.component.scss']
 })
 export class MetricsComponent implements OnInit {
+    metricsList;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.metricsList = metricsList;
+    }
 
 }
+
+
+
+
+
