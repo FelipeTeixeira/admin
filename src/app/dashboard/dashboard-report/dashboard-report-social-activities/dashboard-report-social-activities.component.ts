dff3ec7a50f5714e9aac54fed181b5d0bb54f33d
Felipe Teixeira <felipesantosteixeira@outlook.com>
2019-02-03 19:35:08 -0200

    chart dashboard

--- src/app/dashboard/dashboard-report/dashboard-report-social-activities/dashboard-report-social-activities.component.ts
+++ src/app/dashboard/dashboard-report/dashboard-report-social-activities/dashboard-report-social-activities.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: './dashboard-report.component.html',
-    styleUrls: ['./dashboard-report.component.scss']
+    selector: 'app-dashboard-report-social-activities',
+    templateUrl: './dashboard-report-social-activities.component.html',
+    styleUrls: ['./dashboard-report-social-activities.component.scss']
 })
-export class DashboardReportComponent implements OnInit {
+export class DashboardReportSocialActivitiesComponent implements OnInit {
     // lineChart
     lineChartData: Array<any> = [
         { data: [65, 59, 80, 81, 56, 55, 40], label: 'Facebook' },
@@ -13,17 +14,32 @@ export class DashboardReportComponent implements OnInit {
     ];
     lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     lineChartOptions: any = {
-        responsive: true
+        responsive: true,
+        legend: {
+            labels: {
+                fontFamily: 'Nunito, sans-serif'
+            }
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontFamily: 'Nunito, sans-serif',
+                }
+            }]
+        }
     };
     lineChartColors: Array<any> = [
         {
             backgroundColor: 'rgba(66, 103, 178, 0.4)',
+            borderWidth: 0
         },
         {
             backgroundColor: 'rgba(0, 150, 136, 0.4)',
+            borderWidth: 0
         },
         {
             backgroundColor: 'rgba(29, 161, 242, 0.4)',
+            borderWidth: 0
         }
     ];
     lineChartLegend = true;
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule } from 'ng2-charts';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardMainComponent } from './dashboard-main/dashboard-main.component';
 import { DashboardReportComponent } from './dashboard-report/dashboard-report.component';
+// tslint:disable-next-line:max-line-length
+import { DashboardReportSocialActivitiesComponent } from './dashboard-report/dashboard-report-social-activities/dashboard-report-social-activities.component';
 
 @NgModule({
-    declarations: [DashboardMainComponent, DashboardReportComponent],
+    declarations: [
+        DashboardMainComponent,
+        DashboardReportComponent,
+        DashboardReportSocialActivitiesComponent
+    ],
     imports: [
         CommonModule,
-        DashboardRoutingModule
+        DashboardRoutingModule,
+        ChartsModule
     ]
 })
 export class DashboardModule { }
