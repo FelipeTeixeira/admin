fc116a79c8a4fb5a66b138dcde7f9d7315b25314
Felipe Teixeira <felipesantosteixeira@outlook.com>
2019-02-08 21:55:26 -0200

    interface engagement

--- src/app/fidelity/fidelity.module.ts
+++ src/app/fidelity/fidelity.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FidelityRoutingModule } from './fidelity-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { FidelityEngagementComponent } from './fidelity-engagement/fidelity-engagement.component';
 import { FidelityBuyWinComponent } from './fidelity-buy-win/fidelity-buy-win.component';
 import { FidelityCuponsComponent } from './fidelity-cupons/fidelity-cupons.component';
 
 @NgModule({
-  declarations: [FidelityEngagementComponent, FidelityBuyWinComponent, FidelityCuponsComponent],
-  imports: [
-    CommonModule,
-    FidelityRoutingModule
-  ]
+    declarations: [
+        FidelityEngagementComponent,
+        FidelityBuyWinComponent,
+        FidelityCuponsComponent
+    ],
+    imports: [
+        CommonModule,
+        FidelityRoutingModule,
+        SharedModule
+    ]
 })
 export class FidelityModule { }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,26 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
-import { MaterialModule } from '../shared/material/material.module';
 import { ProductRoutingModule } from './product-routing.module';
 
 import { ProductRegisterComponent } from './product-register/product-register.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductRegisterModalComponent } from './product-register/product-register-modal/product-register-modal.component';
-import { ProductRegisterTableComponent } from './product-register/product-register-table/product-register-table.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
         ProductRegisterComponent,
         ProductListComponent,
-        ProductRegisterModalComponent,
-        ProductRegisterTableComponent
+        ProductRegisterModalComponent
     ],
     imports: [
         CommonModule,
         ProductRoutingModule,
         FormsModule,
-        MaterialModule
+        SharedModule
     ],
     entryComponents: [
         ProductRegisterModalComponent
